af3ac96522d036c1d4e84fd9df8be042ee3ffa9a
CongKTI <congkti@gmail.com>
2025-02-09 22:33:53 +0700

    fix token

--- src/service/config.js
+++ src/service/config.js
@@ -5,6 +5,6 @@ export const http = axios.create({
   timeout: 30000,
   headers: {
     tokenCyberSoft:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA2OCIsIkhldEhhblN0cmluZyI6IjAzLzAyLzIwMjYiLCJIZXRIYW5UaW1lIjoiMTczODU0MDgwMDAwMCIsIm5iZiI6MTcwOTEzOTYwMCwiZXhwIjoxNzM4Njg4NDAwfQ.VqYVXUw3BdF9qomWwcX3sQPDGOUv0fkibw8n-qLHZ2I",
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA2OCIsIkhldEhhblN0cmluZyI6IjE1LzAxLzIwMzAiLCJIZXRIYW5UaW1lIjoiMTczNjg5OTIwMDAwMCIsIm5iZiI6MTcwOTEzOTYwMCwiZXhwIjoxNzM3MDQ2ODAwfQ.9Jr_q1khqkJ7HnLQL_TtGWhQAIBq--dJjj-IK6yeekA",
   },
 });
